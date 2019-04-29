adce75348f86fb6c4899b88e4a5512f0e2884bed
Keith Smith <keith.scotts@gmail.com>
2019-04-29 01:23:52 -0500

    Instantiating IPFS and OrbitDB
    Defining New Piece Please class that instantiates IPFS and OrbitDb instances.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-orbit-db-mvp
+# Orbit DB Minimum Viable Product (MVP)
+
+This repo is to follow the progress of the Orbit DB Field Manual as described at the following repository: <https://github.com/orbitdb/field-manual>
+
+## Chapter 1
+
+Isomorphic Bookends boilerplate loaded.
+
+```javascript
+> const NPP = require('./newPiecePlease')
+undefined
+```
+
+Define Class that creates IPFS and OrbitDb multihash.
+
+```javascript
+> const NPP = require('./newPiecePlease')
+undefined
+Swarm listening on /p2p-circuit/ipfs/Qmes9wAbrcaRn4srBw5ta8dYNCMU9jmBKYBicHjG2WFY92
+> NPP.orbitdb.id
+'Qmes9wAbrcaRn4srBw5ta8dYNCMU9jmBKYBicHjG2WFY92'
+```
--- new_piece_please.js
+++ new_piece_please.js
@@ -2,7 +2,31 @@ const Ipfs = require('ipfs')
 const OrbitDB = require('orbit-db')
 
 class NewPiecePlease {
-  constructor(IPFS, OrbitDB) {}
+  constructor(IPFS, OrbitDB) {
+    this.OrbitDB = OrbitDB
+    this.node = new IPFS({
+      preload: {
+        enabled: false
+      },
+      repo: "./ipfs",
+      EXPERIMENTAL: {
+        pubsub: true
+      },
+      config: {
+        Bootstrap: [],
+        Addresses: {
+          Swarm: []
+        }
+      }
+    });
+
+    this.node.on("error", (e) => { throw (e) })
+    this.node.on("ready", this._init.bind(this))
+  }
+
+  async _init() {
+    this.orbitdb = await this.OrbitDB.createInstance(this.node)
+  }
 }
 
 try {
